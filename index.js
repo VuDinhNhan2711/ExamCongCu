fc46850736204ccfe922797852e7e2ae7d982d8b
lephuocson1999 <lephuocson1999@gmail.com>
2021-03-23 08:20:42 +0700

    create username

---
+++ index.js
@@ -0,0 +1,30 @@
+const express               = require('express');
+const app                   = express();
+const bodyParser            = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('./public'));
+// app.use(express.static(pathToSwaggerUi))
+//e thử comment lại thử nha a okk em
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Enable CORS from client-side
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Expose-Headers', 'Content-Disposition')
+    next()
+  })
+
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    
+});
+
+    
